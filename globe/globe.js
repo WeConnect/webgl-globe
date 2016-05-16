126cc6887982fb647834219ef1c49b10eda324b6
Martin Daguerre <martin.daguerre@wework.com>
2016-05-16 11:06:40 -0300

    Rotation

--- globe/globe.js
+++ globe/globe.js
@@ -78,6 +78,7 @@ DAT.Globe = function(container, opts) {
 
   var mouse = { x: 0, y: 0 }, mouseOnDown = { x: 0, y: 0 };
   var rotation = { x: 0, y: 0 },
+      incr_rotation = { x: -0.003, y: 0 },
       target = { x: Math.PI*3/2, y: Math.PI / 6.0 },
       targetOnDown = { x: 0, y: 0 };
 

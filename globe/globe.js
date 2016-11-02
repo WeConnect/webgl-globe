09da0f32a1190a754e0849c49ddfc12bc0acc43b
Sebastian Martinez <smartang@gmail.com>
2016-11-02 00:25:34 -0300

    Add the ability to customize the color of the point

--- globe/globe.js
+++ globe/globe.js
@@ -189,7 +189,7 @@ DAT.Globe = function(container, opts) {
           lat = data[i];
           lng = data[i + 1];
 
-          color = colorFnWrapper(data,i);
+          color = opts.color || colorFnWrapper(data,i);
           size = 0;
           addPoint(lat, lng, size, color, this._baseGeometry);
         }
@@ -205,7 +205,7 @@ DAT.Globe = function(container, opts) {
     for (i = 0; i < data.length; i += step) {
       lat = data[i];
       lng = data[i + 1];
-      color = colorFnWrapper(data,i);
+      color = opts.color || colorFnWrapper(data,i);
       size = data[i + 2];
       size = size*200;
       addPoint(lat, lng, size, color, subgeo);
